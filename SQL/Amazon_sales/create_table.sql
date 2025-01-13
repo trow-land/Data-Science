812b3c6d0c5928ee5fdb9d41be21e4a04b3c16ac
Tom Rowland <75323342+trow-land@users.noreply.github.com>
2025-01-13 06:41:01 +0000

    Add files via upload

---
+++ SQL/Amazon_sales/create_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE amazon_sales (
+    product_id VARCHAR(255),
+    product_name TEXT,
+    category VARCHAR(100),
+    discounted_price NUMERIC,
+    actual_price NUMERIC,
+    discount_percentage NUMERIC,
+    rating NUMERIC,
+    rating_count INTEGER,
+    about_product TEXT,
+    user_id VARCHAR(255),
+    user_name VARCHAR(100),
+    review_id VARCHAR(255),
+    review_title TEXT,
+    review_content TEXT,
+    img_link TEXT,
+    product_link TEXT
+);
